58d0ff0da4ee25a207fbadfbc779b27a014b7342
Lenny <valentin805@gmail.com>
2018-09-29 17:51:14 +0300

    Calculating a rectangle's perimeter and area through the given coordinates.

---
+++ src/simpleCalculations/RectangleArea.java
@@ -0,0 +1,20 @@
+package simpleCalculations;
+import java.util.Scanner;
+public class RectangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first coordinates: ");
+        double x1 = Double.valueOf(scanner.nextLine());
+        double y1 = Double.valueOf(scanner.nextLine());
+        System.out.println("Enter second coordinates: ");
+        double x2 = Double.valueOf(scanner.nextLine());
+        double y2 = Double.valueOf(scanner.nextLine());
+        double firstSide = Math.abs(x1 - x2);
+        double secondSide = Math.abs(y1 - y2);
+        System.out.println("The sides of the rectangle are "+firstSide+" and "+secondSide+".");
+        double perimeter = 2*(firstSide + secondSide);
+        double area = firstSide*secondSide;
+        System.out.println("Perimeter: "+perimeter);
+        System.out.println("Area: "+area);
+    }
+}
